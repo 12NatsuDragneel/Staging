45b76f234b418c2cc527a3764eccc31cc4e7bb18
Natsu Dragneel <natsu@example.com>
2024-10-25 11:30:51 +0500

    hi

--- src/Components/Latest/Latest.jsx
+++ src/Components/Latest/Latest.jsx
@@ -9,9 +9,9 @@ function Latest() {
         takeSup={"LATEST NEWS"}
         />
         <div className="View1">
-            <div className="View11 View12"> <img src="../../../public/Images/latest-1.jpeg" alt="" /><sup>ARCHITECTURE</sup><p>Target and Amazon Shopping List for Home Stagers</p><a href="#Hero">Read more</a></div>
-            <div className="View11 View13"><img src="../../../public/Images/latest-2.jpeg" alt="" /><sup>INTERIOR</sup><p>6 Ideas for Team Building and Employee Appreciation for Home Stagers</p><a href="#Hero">Read more</a></div>
-            <div className="View11 View14"><img src="../../../public/Images/latest-3.jpeg" alt="" /><sup>PLANNING</sup><p>How to Find the Best Price Structure for Your Home Staging Services</p><a href="#Hero">Read more</a></div>
+            <div className="View11 View12"> <img src="./Images/latest-1.jpeg" alt="" /><sup>ARCHITECTURE</sup><p>Target and Amazon Shopping List for Home Stagers</p><a href="#Hero">Read more</a></div>
+            <div className="View11 View13"><img src="./Images/latest-2.jpeg" alt="" /><sup>INTERIOR</sup><p>6 Ideas for Team Building and Employee Appreciation for Home Stagers</p><a href="#Hero">Read more</a></div>
+            <div className="View11 View14"><img src="./Images/latest-3.jpeg" alt="" /><sup>PLANNING</sup><p>How to Find the Best Price Structure for Your Home Staging Services</p><a href="#Hero">Read more</a></div>
         </div>
     </div>
   )
